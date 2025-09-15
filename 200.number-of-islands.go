3a6b2710bb41943f71bac9747566431abb376510
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2025-09-15 23:57:53 +0300

    200 task is done

---
+++ 200.number-of-islands.go
@@ -0,0 +1,23 @@
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] == '0' {
+		return
+	}
+	grid[i][j] = '0'
+	dfs(grid, i-1, j)
+	dfs(grid, i+1, j)
+	dfs(grid, i, j-1)
+	dfs(grid, i, j+1)
+}
+
+func numIslands(grid [][]byte) int {
+	ans := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == '1' {
+				dfs(grid, i, j)
+				ans++
+			}
+		}
+	}
+	return ans
+}
